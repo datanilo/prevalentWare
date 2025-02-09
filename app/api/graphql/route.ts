baee13016d366c375e73241c3a26ea7097d7c80b
Danilo Méndez <bd.mendez10@uniandes.edu.co>
2025-02-09 01:31:25 -0500

    Update route.ts

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,17 +1,12 @@
-// app/api/graphql/route.ts
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { auth } from '@/lib/auth'; // Importa tu middleware de autenticación
+import { auth } from '@/lib/auth';
 import { NextResponse } from 'next/server';
 import {schema} from '@/lib/schema';
 
 
-// Inicializa el ApolloServer con el esquema
 const server = new ApolloServer({ schema });
-
-// Crea el handler de Next.js para Apollo Server
 const nextHandler = startServerAndCreateNextHandler(server);
-
 
 export const GET = auth(async (req) => {
   if (!req.auth) {
@@ -20,10 +15,6 @@ export const GET = auth(async (req) => {
   return nextHandler(req);
 });
 
-/**
- * Exporta el handler para el método POST.
- * Se protege el endpoint de la misma forma que el GET.
- */
 export const POST = auth(async (req) => {
   if (!req.auth) {
     return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
